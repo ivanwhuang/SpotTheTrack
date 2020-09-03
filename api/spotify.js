17d4419675080ebcd2ba0d5373bf30e9da02086c
Jesse Lee <jesselee613@gmail.com>
2020-09-03 15:24:24 -0700

    Update api to use spotify wrapper

--- api/spotify.js
+++ api/spotify.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
 const request = require('request');
 const express = require('express');
-const fs = require('fs');
+const dotenv = require('dotenv').config();
+const Spotify = require('node-spotify-api');
 const router = express.Router();
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_CLIENT,
+  secret: process.env.SPOTIFY_SECRET,
+});
 
 // @route   GET api/spotify
 // @desc    Sample route for Jesse Lee
@@ -22,53 +28,31 @@ router.get('/test', async (req, res) => {
 //          with a random keyword from a predefined list
 // @access  Private
 router.get('/gettrack', (req, res) => {
-  let token = fs.readFileSync(
-    '.env.development.local', 
-    {
-      encoding: 'utf8',
-      flag: 'r'
-  });
-  let url = "https://api.spotify.com/v1/search";
-  let query = {
-    q: 'illenium',
-    type: 'track',
-    limit: '1'
-  };
-
+  
   // NOTE: prototype for choosing a random artist
-  let artists = Array("Said%20the%20sky", "illenium", "dabin", "Calvin%20Harris");
+  let artists = Array("Said the sky", "illenium", "dabin", "Calvin Harris");
   let rand_keyword = artists[Math.floor(Math.random() * artists.length)];
-
-  request.get(
-    `https://api.spotify.com/v1/search?q=${rand_keyword}&type=track&limit=1&market=US`, 
+  console.log(rand_keyword);
+  spotify.search(
     {
-      'auth': {
-        'bearer': token
-    }
-  }, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-    }
-
-    if (response.statusCode == 200) {
-      // TODO: filter for preview url and append to json response along with name
-      let bodyAsJson = JSON.parse(body);
-      // console.log(bodyAsJson['tracks']['items'][0]['preview_url']);
+      type: 'track',
+      query: `${rand_keyword}`,
+      limit: '1',
+    },
+    function (err, data) {
+      if (err) {
+        return console.log('Error occured:' + err);
+      }
+      console.log(data['tracks']['items'][0]['preview_url']);
       res.json(
         JSON.stringify(
-          {
-            "name": bodyAsJson['tracks']['items'][0]['name'],
-            "preview": bodyAsJson['tracks']['items'][0]['preview_url']
-          })
+        {
+          "name": data['tracks']['items'][0]['name'],
+          "preview": data['tracks']['items'][0]['preview_url']
+        })
       );
     }
-    else {
-      console.error(`Error: ${response.statusCode}`);
-      res.status(response.statusCode).send('Request Error');
-    }
-    
-  });
+  );
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
-const Spotify = require('node-spotify-api');
 
 const app = express();
 
@@ -10,25 +8,6 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/spotify', require('./api/spotify'));
-
-// FOR JESSE LEE - example spotify search
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_CLIENT,
-    secret: process.env.SPOTIFY_SECRET,
-});
-
-spotify.search(
-    {
-        type: 'track',
-        query: 'Without you Avicii',
-    },
-    function (err, data) {
-        if (err) {
-            return console.log('Error occured:' + err);
-        }
-        console.log(data);
-    }
-);
 
 const PORT = process.env.PORT || 5000;
 
